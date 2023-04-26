cf25f3e77d2b45497b70f81120c96e799c2f2225
Daniel Felipe Cosme <pipecosme123@gmail.com>
2023-04-26 13:52:23 -0500

    Visualizador para las instituciones

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import Formulario from './paginas/Formulario';
 import Odontologo from './paginas/Odontologo';
 
 import './css/App.css';
-import Insituciones from './paginas/Insituciones';
+// import Insituciones from './paginas/Insituciones';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path={RoutersLinks.Formulario} element={<Formulario />} />
+        <Route exact path={RoutersLinks.Form_Colaboradores} element={<Formulario colaborador={true} />} />
         <Route exact path={RoutersLinks.Odontologos} element={<Odontologo />} />
-        <Route exact path={RoutersLinks.Insituciones} element={<Insituciones />} />
+        {/* <Route exact path={RoutersLinks.Insituciones} element={<Insituciones />} /> */}
       </Routes>
     </BrowserRouter>
   );
--- src/paginas/Insituciones.js
+++ src/paginas/Insituciones.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import logo from '../css/img/Logo.svg';
+import mural from '../css/img/Imagen_BG_smile.png';
+
+import '../css/Insituciones.css';
 
 const Insituciones = () => {
+
+   window.document.title = 'Visualizaciones - Pinta un mural';
+
    return (
       <div className='Insituciones'>
-         Página en construcción - Insituciones
+         <div className="logo">
+            <img src={logo} alt="" />
+         </div>
+         <div className="content">
+            <img className='mural' src={mural} alt="" />
+            <h4>Las fotos de los murales se comenzarán a subir a partir del día <b>08 de mayo</b></h4>
+         </div>
       </div>
    );
 };
