4506208bb18cdd6feff0dcd854f05a2324b2908e
Daniel Felipe Cosme <pipecosme123@gmail.com>
2023-06-01 07:18:17 -0500

    Avance

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RoutersLinks } from './constantes/RoutersLinks';
 import Formulario from './paginas/Formulario';
-// import Odontologo from './paginas/Odontologo';
+import { ThemeProvider, createTheme } from '@mui/material';
 
 import './css/App.css';
-import Insituciones from './paginas/Insituciones';
+import ViewDrawings from './paginas/ViewDrawings';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path={RoutersLinks.Form_Odontologos} element={<Formulario tipo={'odontologos'} />} />
-        <Route exact path={RoutersLinks.Form_Colaboradores} element={<Formulario tipo={'empleados'} />} />
-        <Route exact path={RoutersLinks.View_Odontologos} element={<Insituciones tipo={'odontologos'} />} />
-        {/* <Route exact path={RoutersLinks.View_Odontologos} element={<Odontologo tipo={'odontologos'} />} /> */}
-        <Route exact path={RoutersLinks.View_Colaboradores} element={<Insituciones tipo={'empleados'} />} />
-        {/* <Route exact path={RoutersLinks.View_Colaboradores} element={<Odontologo tipo={'empleados'} />} /> */}
-        {/* <Route exact path={RoutersLinks.Insituciones} element={<Insituciones />} /> */}
-      </Routes>
-    </BrowserRouter>
-  );
+
+   const theme = createTheme({
+      palette: {
+         primary: {
+            main: '#27d2f0',
+         },
+         secondary: {
+            main: '#82bb00',
+         },
+         third: {
+            main: '#d2010d',
+         }
+      },
+      overrides: {
+         MuiInput: {
+            underline: {
+               '&:before': {
+                  borderBottom: '2px solid rgba(0, 0, 0, 0.42)',
+               },
+               '&:hover:not($disabled):before': {
+                  borderBottom: `2px solid #27d2f0`,
+               },
+               '&:after': {
+                  borderBottom: `2px solid #27d2f0`,
+               },
+            },
+         },
+      },
+      typography: {
+         fontFamily: ["Light", "Regular", "Bold"].join(',')
+      },
+   });
+
+   return (
+      <ThemeProvider theme={theme}>
+         <BrowserRouter>
+            <Routes>
+               <Route exact path={RoutersLinks.Form_Odontologos} element={<Formulario tipo={'odontologos'} />} />
+               <Route exact path={RoutersLinks.Form_Colaboradores} element={<Formulario tipo={'empleados'} />} />
+               <Route exact path={RoutersLinks.View_Odontologos} element={<ViewDrawings tipo={'odontologos'} titulo={'Odontólogos y Pacientes'} />} />
+               <Route exact path={RoutersLinks.View_Colaboradores} element={<ViewDrawings tipo={'empleados'} titulo={'Colaboradores de Colgate'} />} />
+               {/* <Route exact path={RoutersLinks.View_Odontologos} element={<Odontologo tipo={'odontologos'} />} /> */}
+               {/* <Route exact path={RoutersLinks.View_Colaboradores} element={<Insituciones tipo={'empleados'} />} /> */}
+               {/* <Route exact path={RoutersLinks.View_Colaboradores} element={<Odontologo tipo={'empleados'} />} /> */}
+               {/* <Route exact path={RoutersLinks.Insituciones} element={<Insituciones />} /> */}
+            </Routes>
+         </BrowserRouter>
+      </ThemeProvider >
+   );
 }
 
 export default App;
--- src/constantes/RoutersLinks.js
+++ src/constantes/RoutersLinks.js
@@ -7,5 +7,5 @@ export const RoutersLinks = {
 }
 
 // export const urlApi = "http://localhost:3012";
-// export const urlApi = "http://192.168.1.29:3012";
-export const urlApi = "https://dibuja-y-colorea-sbfb.col1.co/api";
+export const urlApi = "http://192.168.1.29:3012";
+// export const urlApi = "https://dibuja-y-colorea-sbfb.col1.co/api";
